5ed141811a69fffd17a5b395982f2638716dbdd7
katyjohn124 <148299728+katyjohn124@users.noreply.github.com>
2024-01-12 14:39:08 +0800

    修改

--- 中上实战班js练习/async-await.js
+++ 中上实战班js练习/async-await.js
@@ -1,12 +1,12 @@
-//ice-cream little project about 异步编程：回调、promise、async/await
-let stocks = {
-    fruits: ["strawberry", "grapes", "banana", "apple", "orange"],
-    liquid: ["water", "ice"],
-    holder: ["cone", "cup", "stick"],
-    toppings: ["chocolate", "peanuts"],
+// //ice-cream little project about 异步编程：回调、promise、async/await
+// let stocks = {
+//     fruits: ["strawberry", "grapes", "banana", "apple", "orange"],
+//     liquid: ["water", "ice"],
+//     holder: ["cone", "cup", "stick"],
+//     toppings: ["chocolate", "peanuts"],
 
-}
-//回调地狱例子
+// }
+// 回调地狱例子
 // let order = (fruit_name, call_production) => {
 //     setTimeout(function () {
 //         console.log(`${stocks.fruits[fruit_name]} was selected!`);
@@ -27,58 +27,58 @@ let stocks = {
 //     }, 1000);
 // }
 
-//回调地狱的代码看起来很乱，可读性和可维护性较差
+// //回调地狱的代码看起来很乱，可读性和可维护性较差
 // order(3, production);
 
 
 
-//promise重写冰淇淋店
-let is_shop_open = false;
+// //promise重写冰淇淋店
+// let is_shop_open = false;
 
-let order = (time, work) => {
-    return new Promise((resolve, reject) => {
-        if (is_shop_open) {
-            setTimeout(() => {
-                resolve(work());
-            }, time);
+// let order = (time, work) => {
+//     return new Promise((resolve, reject) => {
+//         if (is_shop_open) {
+//             setTimeout(() => {
+//                 resolve(work());
+//             }, time);
 
-        } else {
-            reject(console.log('our shop is close'));
-        }
-    })
-}
+//         } else {
+//             reject(console.log('our shop is close'));
+//         }
+//     })
+// }
 
-order(2000, () => console.log(`${stocks.fruits[2]} was selected!`))
+// order(2000, () => console.log(`${stocks.fruits[2]} was selected!`))
 
-    .then(() => {
-        return order(1000, () => console.log('production has been started'))
-    })
-    .then(() => {
-        return order(2000, () => console.log('fruits was chopped'))
-    })
-    .then(() => {
-        return order(1000, () => console.log('water and ice was added'))
-    })
-    .then(() => {
-        return order(1000, () => console.log('machine has been started'))
-    })
-    .then(() => {
-        return order(2000, () => console.log('container has been selected'))
-    })
-    .then(() => {
-        return order(3000, () => console.log('toppings has been selected'))
-    })
-    .then(() => {
-        return order(2000, () => console.log('serve ice cream'))
-    })
+//     .then(() => {
+//         return order(1000, () => console.log('production has been started'))
+//     })
+//     .then(() => {
+//         return order(2000, () => console.log('fruits was chopped'))
+//     })
+//     .then(() => {
+//         return order(1000, () => console.log('water and ice was added'))
+//     })
+//     .then(() => {
+//         return order(1000, () => console.log('machine has been started'))
+//     })
+//     .then(() => {
+//         return order(2000, () => console.log('container has been selected'))
+//     })
+//     .then(() => {
+//         return order(3000, () => console.log('toppings has been selected'))
+//     })
+//     .then(() => {
+//         return order(2000, () => console.log('serve ice cream'))
+//     })
 
-    .catch(() => {
-        console.log('custom was left!')
-    })
+//     .catch(() => {
+//         console.log('custom was left!')
+//     })
 
-//async/await 重写冰淇淋店
+// // async/await 重写冰淇淋店
 // let is_shop_open = true;
-//创建时间函数
+// 创建时间函数
 // function time(ms) {
 //     return new Promise((resolve, reject) => {
 //         if (is_shop_open) {
